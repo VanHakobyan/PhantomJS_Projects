f5e67806e9c3397580e4875af6186aa29554910a
van hakobyan <van19962013@mail.ru>
2018-05-07 18:08:07 +0400

    init

---
+++ StartingStady/FirstApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NReco.PhantomJS;
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var phantomJs = new PhantomJS();
+            phantomJs.OutputReceived += (sender, e) => {
+                Console.WriteLine($"PhantomJS output: {e.Data}");
+            };
+            phantomJs.RunScript("for (var i=0; i<10; i++) console.log('hello from js '+i); phantom.exit();", null);
+        }
+    }
+}
